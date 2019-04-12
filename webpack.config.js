3152c3e8af00de761eeb68bac7eaf12267453091
Syed M Sohaib <muhammad.sohaib@salsoft.net>
2019-04-12 15:52:55 +0500

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/js', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'build.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader','css-loader'],
+            include: /src/
+        },{
+            test: /\.jsx?$/,
+            use: ['babel-loader'],
+            exclude: /node_modules/,
+            query: {
+                preset: ['es2015','react','stage-2']
+            }
+
+        }]
+    }
+
+}
